8b8fdb0b40815dce80b03f6d159170a11555c450
Abdi-01 <abdialghi@gmail.com>
2021-12-06 21:09:36 +0700

    verification page with keeplogin

--- src/pages/VerificationPage.js
+++ src/pages/VerificationPage.js
@@ -1,18 +1,45 @@
 import React from 'react';
 import image_verified from './undraw_verified.png';
 import { Button } from 'reactstrap'
+import axios from 'axios';
+import { API_URL } from '../helper'
+import { keepLogin } from '../actions'
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 
 class VerificationPage extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {
+            redirect: false
+        }
     }
 
     onBtVerification = () => {
         console.log(window.location)
+        let token = window.location.pathname.split('/')[2]
+        console.log("TOKEN URL", token)
+
+        axios.patch(`${API_URL}/users/verified`, {}, {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        }).then((res) => {
+            console.log(res.data)
+            if (res.data.success) {
+                this.props.keepLogin(token)
+                this.setState({ redirect: true })
+                alert(res.data.message)
+            }
+        }).catch((err) => {
+            console.log(err)
+        })
     }
 
     render() {
+        if (this.state.redirect) {
+            return <Redirect to="/" />
+        }
         return (
             <div className="container p-5">
                 <div className="shadow p-5 text-center rounded">
@@ -27,4 +54,4 @@ class VerificationPage extends React.Component {
     }
 }
 
-export default VerificationPage;
+export default connect(null, { keepLogin })(VerificationPage);
